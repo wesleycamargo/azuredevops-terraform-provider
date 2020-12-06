7f2a0e3f2d35a2b882d60dc3327df112193efca6
Wesley Camargo <camargo.wes@gmail.com>
2020-12-06 18:27:33 +0100

    added service connection

--- variables.tf
+++ variables.tf
@@ -21,3 +21,27 @@ variable "repository_name" {
 variable "repository_template" {
   default = "__repository_template__"
 }
+
+variable "serviceprincipalid" {
+  default = "__serviceprincipalid__" 
+}
+
+variable "serviceprincipalkey" {
+  default = "__serviceprincipalkey__" 
+}
+
+variable "azurerm_spn_tenantid" {
+  default = "__azurerm_spn_tenantid__" 
+}
+
+variable "azurerm_subscription_id" {
+  default = "__azurerm_subscription_id__" 
+}
+
+variable "azurerm_subscription_name" {
+  default = "__azurerm_subscription_name__" 
+}
+
+variable "service_endpoint_name" {
+  default = "__service_endpoint_name__" 
+}
