c3f35d9ddde9f66cb9c26af8f210e2617c14039d
Wesley Camargo <camargo.wes@gmail.com>
2020-12-09 21:41:21 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,42 +6,6 @@ variable azdo_url {
     default = "__azdo_url__"
 }
 
-variable pr_approver {
-    default = "__pr_approver__"
-}
-
 variable "teamproject_name" {
   default = "__teamproject_name__"
-}
-
-variable "repository_name" {
-  default = "__repository_name__"
-}
-
-variable "repository_template" {
-  default = "__repository_template__"
-}
-
-variable "serviceprincipalid" {
-  default = "__serviceprincipalid__" 
-}
-
-variable "serviceprincipalkey" {
-  default = "__serviceprincipalkey__" 
-}
-
-variable "azurerm_spn_tenantid" {
-  default = "__azurerm_spn_tenantid__" 
-}
-
-variable "azurerm_subscription_id" {
-  default = "__azurerm_subscription_id__" 
-}
-
-variable "azurerm_subscription_name" {
-  default = "__azurerm_subscription_name__" 
-}
-
-variable "service_endpoint_name" {
-  default = "__service_endpoint_name__" 
 }
